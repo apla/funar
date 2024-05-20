061e633505c85f008b1f70fa885728df1e342a6e
Ivan Baktsheev <dot.and.thing@gmail.com>
2024-05-20 23:16:35 +0300

    feat: generator generating

---
+++ test/003-cli-generator.js
@@ -0,0 +1,103 @@
+import { exec as execWithCb } from "node:child_process";
+import { promisify } from "node:util";
+import assert from "node:assert";
+import { describe, it, before } from "node:test";
+import { writeFile } from "node:fs/promises";
+
+const exec = promisify(execWithCb);
+
+/**
+ * Async exec wrapper to provide stdout or throw an exception
+ * @param {string} command command to execute
+ * @returns {Promise<string>}
+ */
+async function execCmd(command) {
+	try {
+		const { stdout, stderr } = await exec(command);
+		return stdout;
+	} catch (error) {
+		throw new Error(`Command failed with error code ${error.code}: ${error.stdout} ${error.stderr}`);
+	}
+}
+
+describe("cli generator", function() {
+
+	before(async () => {
+		await writeFile("./test/fixtures/cli-input.js", `
+/**
+ * Echo a and b
+ * @param {string} a a
+ * @param {string} b b
+ * */
+export function testAB (a, b) {
+	console.log(a);
+	console.log(b);
+}
+
+/**
+ * Sum 3 numbers
+ * @param {Object} options
+ * @param {number} options.x x
+ * @param {number} options.y y
+ * @param {number} options.z z
+ * */
+export function testXYZ ({x, y, z}) {
+	// x, y, and z are actually numbers
+	const result = x + y + z;
+	// avoid nodejs util.inspect number ascii color wrapping
+	console.log(result.toString());
+}
+
+		`);
+	});
+
+	it("should return stdout for a successful command", async () => {
+		const result = await execCmd("echo Hello World");
+		assert.strictEqual(result.trim(), "Hello World");
+	});
+
+	it("should throw an error for a failing command", async function() {
+		try {
+			await execCmd("nonexistent-command");
+			// If the above line does not throw, the test should fail
+			assert.fail("Expected error was not thrown");
+		} catch (error) {
+			assert.strictEqual(error.message.includes("Command failed with error code"), true);
+		}
+	});
+
+	it("should generate a file from example", async () => {
+
+		await execCmd("node ./bin/funar.js gen -t cli -i ./test/fixtures/cli-input.js -o ./test/fixtures/cli-output.js");
+
+	});
+
+	it("should display a list of commands", async () => {
+
+		try {
+			await execCmd("node ./test/fixtures/cli-output.js");
+		} catch (error) {
+			assert(error.message.includes("Echo a and b"));
+			assert(error.message.includes("Sum 3 numbers"));
+		}
+
+	});
+
+	it("should echo a and b", async () => {
+
+		const testABOut = await execCmd("node ./test/fixtures/cli-output.js testAB --a hello --b world");
+
+		assert.strictEqual(testABOut.trim(), "hello\nworld");
+
+	});
+
+	it("should sum 3 numbers", async () => {
+
+		const testXYZOut = await execCmd("node ./test/fixtures/cli-output.js testXYZ --x 25 --y 50 --z 75");
+
+		assert.strictEqual(testXYZOut.trim(), "150");
+
+	});
+
+
+});
